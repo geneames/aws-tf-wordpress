5c876d7950231330f80398ccbb30e8112e31a099
geneames <games@ancestry.com>
2019-04-21 15:51:50 -0700

    More work

--- deployments/dev/bastion-hosts/main.tf
+++ deployments/dev/bastion-hosts/main.tf
@@ -2,7 +2,20 @@
 # Local Variables
 ######################################################
 locals {
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.name}"
 
+
+  userdata = <<USERDATA
+    #!/bin/bash
+    cat <<"__EOF__" > /home/ec2-user/.ssh/config
+    Host *
+        StrictHostKeyChecking no
+    __EOF__
+    chmod 600 /home/ec2-user/.ssh/config
+    chown ec2-user:ec2-user /home/ec2-user/.ssh/config
+  USERDATA
 }
 
 ######################################################
@@ -16,7 +29,7 @@ data "aws_vpc" "vpc" {
 
   filter {
     name = "tag:Name"
-    values = ["${module.label.namespace}-${module.label.stage}-${module.label.name}"]
+    values = ["${local.namespace}-${local.stage}-${local.name}"]
   }
 }
 
@@ -39,26 +52,31 @@ data "aws_subnet_ids" "subnets_for_asg" {
   }
 }
 
-######################################################
-# MODULES
-######################################################
+module "bastion-asg" {
+  source = "github.com/geneames/terraform-aws-ec2-autoscale-group.git?ref=0.1"
 
-module "label" {
-  source     = "git::https://github.com/geneames/terraform-aws-null-label.git?ref=tags/0.1"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${var.tags}"
-  enabled    = "${var.enabled}"
-}
+  namespace                   = "${local.namespace}"
+  name                        = "${local.name}"
+  stage                       = "${local.stage}"
 
-//module "bashtion-asg" {
-//  source = "github.com/geneames/terraform-aws-ec2-autoscale-group.git?ref=0.1"
-//
-//}
+  image_id                    = "${var.image_id}"
+  instance_type               = "${var.instance_type}"
+  security_group_ids          = []
+  subnet_ids                  = ["${data.aws_subnet_ids.subnets_for_asg.ids}"]
+  health_check_type           = "${var.health_check_type}"
+  min_size                    = "${var.min_size}"
+  max_size                    = "${var.max_size}"
+  wait_for_capacity_timeout   = "${var.wait_for_capacity_timeout}"
+  associate_public_ip_address = true
+  user_data_base64            = "${base64encode(local.userdata)}"
 
-output "" {
-  value = "${data.aws_subnet_ids.subnets_for_asg.ids}"
+  tags = {
+    Tier              = "dmz"
+    Bastion-Cluster = "${var.region}-${local.namespace}-${local.stage}-${local.name}"
+  }
+
+  # Auto-scaling policies and CloudWatch metric alarms
+  autoscaling_policies_enabled           = "true"
+  cpu_utilization_high_threshold_percent = "${var.cpu_utilization_high_threshold_percent}"
+  cpu_utilization_low_threshold_percent  = "${var.cpu_utilization_low_threshold_percent}"
 }
